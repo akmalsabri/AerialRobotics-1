db40d77e224b476974b3eceba77a368121c364eb
Nishanth Koganti <buntyke@gmail.com>
2016-02-01 16:51:14 +0900

    [Week2] complete programming assignment

--- 1DControl/controller.m
+++ 1DControl/controller.m
@@ -5,11 +5,11 @@ function [ u ] = pd_controller(~, s, s_des, params)
 %   s_des: 2x1 vector containing desired state [z; v_z]
 %   params: robot parameters
 
-u = 0;
-
-
 % FILL IN YOUR CODE HERE
 
+Kp = 150;
+Kv = 20;
+u = params.mass*(Kp*(s_des(1)-s(1))+Kv*(s_des(2)-s(2))+params.gravity);
 
 end
 
